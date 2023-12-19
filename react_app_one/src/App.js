80c2d9b1db03e2e6d5da09f6ca37ef3969eeed49
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2023-12-19 11:41:18 +0600

    expnese items

--- react_app_one/src/App.js
+++ react_app_one/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ExpenseItems from './components/ExpenseItems'
 function App() {
   return (
     <div>
       <h1>Hello world !!</h1>
+      <ExpenseItems />
     </div>
   );
 }
