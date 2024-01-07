422ebcf32b62e915525d4c839f659a5aea9ed218
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2024-01-07 22:37:09 +0600

    done

--- 11 - Practice Project_ Building a Food Order App/src/App.js
+++ 11 - Practice Project_ Building a Food Order App/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Header from "./components/Layouts/Header.js";
+import Meals from "./components/Meals/Meals.js";
 function App() {
   return (
     <div>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </div>
   );
 }
