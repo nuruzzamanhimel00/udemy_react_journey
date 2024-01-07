7788eb884ce6c5c011e7643f82ed6402f679df26
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2024-01-07 12:07:37 +0600

    done

--- advaced_handling/src/App.js
+++ advaced_handling/src/App.js
@@ -4,6 +4,8 @@ import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
 
+import AuthContext from "./store/auth-context.js";
+
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   // console.log("1");
@@ -28,13 +30,23 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
+    <>
+      <AuthContext.Provider
+        value={{
+          isLoggedIn: isLoggedIn,
+          onLogout: logoutHandler,
+          onLogin: loginHandler,
+        }}
+      >
+        <MainHeader />
+        <main>
+          {!isLoggedIn && <Login />}
+          {/* {!isLoggedIn && <Login onLogin={loginHandler} />} */}
+          {/* {isLoggedIn && <Home onLogout={logoutHandler} />} */}
+          {isLoggedIn && <Home />}
+        </main>
+      </AuthContext.Provider>
+    </>
   );
 }
 
--- advaced_handling/src/components/Login/Login.js
+++ advaced_handling/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
 
 const Login = (props) => {
   // const [enteredEmail, setEnteredEmail] = useState("");
@@ -10,6 +11,8 @@ const Login = (props) => {
   const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
+
+  const ctx = useContext(AuthContext);
   // // console.log("1");
   // useEffect(() => {
   //   console.log("2- User effect running");
@@ -116,7 +119,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    ctx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
