8b81c9e45a071b15de3ea0d1b98dc76dd6157ed1
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2024-01-30 00:41:29 +0600

    done

--- 11_food_order/src/components/Layouts/Header.js
+++ 11_food_order/src/components/Layouts/Header.js
@@ -1,15 +1,16 @@
 import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton.js";
 
 const Header = () => {
   return (
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <p>Cart</p>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
-        <img src={mealsImage} />
+        <img src={mealsImage} alt="test" />
       </div>
     </>
   );
---
+++ 11_food_order/src/components/Layouts/HeaderCartButton.js
@@ -0,0 +1,18 @@
+import CartIcon from "../Cart/CartIcon.js";
+import classes from "./HeaderCartButton.module.css";
+
+const HeaderCartButton = () => {
+  return (
+    <>
+      <button className={classes.button}>
+        <span className={classes.icon}>
+          <CartIcon />
+        </span>
+        <span>Your Cart</span>
+        <span className={classes.badge}>10</span>
+      </button>
+    </>
+  );
+};
+
+export default HeaderCartButton;
