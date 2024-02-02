b15b24a34d6ecc9ca9c97d3cd81b14a151d783cc
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2024-02-02 12:04:46 +0600

    done

--- 11_food_order/src/App.js
+++ 11_food_order/src/App.js
@@ -1,8 +1,12 @@
 import Header from "./components/Layouts/Header";
+import Meals from "./components/Meals/Meals";
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
---
+++ 11_food_order/src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,18 @@
+import classes from "./MealItem.module.css";
+
+const MealItem = (props) => {
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>${props.price.toFixed(2)}</div>
+      </div>
+      <div>
+        {/* <MealItemForm id={props.id} onAddToCart={addToCartHandler} /> */}
+      </div>
+    </li>
+  );
+};
+
+export default MealItem;
