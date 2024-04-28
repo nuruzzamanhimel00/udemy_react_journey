2a5b59bb0a378faeaed32bac49345204a5386220
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2024-04-29 00:48:57 +0600

    react learn completed

--- 22_auth/src/store/auth-context.js
+++ 22_auth/src/store/auth-context.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-
+import React, {useState, useEffect} from 'react';
+let logoutTimer = null
 const AuthContext = React.createContext({
     token: '',
     isLoggedIn: false,
@@ -11,26 +11,53 @@ const calculateReminIngTime = (expirationTime) => {
     const currentTime = new Date().getTime();
     const adjExpirationTime = new Date(expirationTime).getTime();
     const remianingTime = adjExpirationTime - currentTime
-    console.log(remianingTime, currentTime, adjExpirationTime)
+    // console.log(remianingTime, currentTime, adjExpirationTime)
     return remianingTime
+}
+
+const retriveStoreToken = () => {
+    const storeToken = (localStorage.getItem('token'));
+    const expireTime = localStorage.getItem('expireTime')
+    const reminingTime = calculateReminIngTime(expireTime)
+    if (reminingTime <= 3000) {
+        localStorage.removeItem('token')
+        localStorage.removeItem('expireTime')
+        return null;
+    }
+    return {
+        token: storeToken,
+        expireTime: expireTime,
+        duration: reminingTime
+    };
 }
 
 
 export const AuthContextProvider = (props) => {
-
-    const [token, setToken] = useState(localStorage.getItem('token'))
+    const tokenData = retriveStoreToken()
+    const [token, setToken] = useState(tokenData !== null && tokenData ? tokenData.token : null)
     
     const isLoggedIn = token ? true : false
+
+    useEffect(() => {
+        if (tokenData && tokenData !== null) {
+            console.log('duration',tokenData.duration)
+            logoutTimer = setTimeout(logoutHandler, tokenData.duration);
+        }
+    },[tokenData])
     const loginHandler = (token, expireTime) => {
         setToken(token)
         localStorage.setItem('token', token)
+        localStorage.setItem('expireTime', expireTime)
 
         const reminingTime = calculateReminIngTime(expireTime)
-        setTimeout(logoutHandler, reminingTime)
+        logoutTimer = setTimeout(logoutHandler, reminingTime)
     }
     const logoutHandler = () => {
         setToken(null)
         localStorage.removeItem('token')
+        if (logoutTimer !== null && logoutTimer) {
+            clearTimeout(logoutTimer)
+        }
     }
 
     const initaialValue = {
